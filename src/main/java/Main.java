066580552c187ce74ed887936ee8a6d4ffd5e648
danielpopusoi <daniel.popusoi96@gmail.com>
2024-07-27 22:57:17 +0300

    Bind a port

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
@@ -9,17 +8,16 @@ public class Main {
 
     // Uncomment this block to pass the first stage
     //
-    // try {
-    //   ServerSocket serverSocket = new ServerSocket(4221);
-    //
-    //   // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    //   // ensures that we don't run into 'Address already in use' errors
-    //   serverSocket.setReuseAddress(true);
-    //
-    //   serverSocket.accept(); // Wait for connection from client.
-    //   System.out.println("accepted new connection");
-    // } catch (IOException e) {
-    //   System.out.println("IOException: " + e.getMessage());
-    // }
+     try (ServerSocket serverSocket = new ServerSocket(4221)) {
+
+       // Since the tester restarts your program quite often, setting SO_REUSEADDR
+       // ensures that we don't run into 'Address already in use' errors
+       serverSocket.setReuseAddress(true);
+
+       serverSocket.accept(); // Wait for connection from client.
+       System.out.println("accepted new connection");
+     } catch (IOException e) {
+       System.out.println("IOException: " + e.getMessage());
+     }
   }
 }
