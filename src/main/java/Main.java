cad139fa2e0e6355cbb2d7290e3c7bc74126019c
danielpopusoi <daniel.popusoi96@gmail.com>
2024-07-27 23:34:44 +0300

    Respond with 200

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,26 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
+
+  private static final String CRLF = "\r\n";
+
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // You can use print statements as follows for debugging, they'll be visible
+    // when running tests.
     System.out.println("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
-     try (ServerSocket serverSocket = new ServerSocket(4221)) {
-
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
-
-       serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
+    Socket clientSocket = null;
+    try (ServerSocket serverSocket = new ServerSocket(4221)) {
+      // Since the tester restarts your program quite often, setting
+      // SO_REUSEADDR ensures that we don't run into 'Address already in use'
+      // errors
+      serverSocket.setReuseAddress(true);
+      clientSocket = serverSocket.accept(); // Wait for connection from client.
+      System.out.println("accepted new connection");
+      clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    }
   }
 }
