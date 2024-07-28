dcb4ac5b6ae95b38440e065f626fae91d9bea687
danielpopusoi <daniel.popusoi96@gmail.com>
2024-07-28 15:34:03 +0300

    Extract URL path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,50 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
   private static final String CRLF = "\r\n";
 
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible
-    // when running tests.
-    System.out.println("Logs from your program will appear here!");
     Socket clientSocket = null;
     try (ServerSocket serverSocket = new ServerSocket(4221)) {
-      // Since the tester restarts your program quite often, setting
-      // SO_REUSEADDR ensures that we don't run into 'Address already in use'
-      // errors
+
       serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept(); // Wait for connection from client.
-      System.out.println("accepted new connection");
-      clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+      clientSocket = serverSocket.accept(); // Wait for connection from the client.
+
+      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      List<String> requestData = readRequest(in);
+      String path = requestData.get(0);
+      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+      writeResponseByPath(out, path);
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
+  }
+
+  private static void writeResponseByPath(PrintWriter out, String path) {
+    switch (path) {
+      case "GET / HTTP/1.1" -> out.println("HTTP/1.1 200 OK\r\n\r\n");
+      default -> out.println("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+  }
+
+  private static List<String> readRequest(BufferedReader in) throws IOException {
+    List<String> requestData = new ArrayList<>();
+    String inputLine;
+    while ((inputLine = in.readLine()) != null) {
+      System.out.println(inputLine);
+      requestData.add(inputLine);
+      if (inputLine.isEmpty()) {
+        break;
+      }
+    }
+    return requestData;
   }
 }
