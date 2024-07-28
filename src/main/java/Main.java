18cf9b4c264f3a9d3f1e6f2b318beabfb7bcb3d7
danielpopusoi <daniel.popusoi96@gmail.com>
2024-07-28 23:00:49 +0300

    return a file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,17 +17,20 @@ public class Main {
 
   private static final String USER_AGENT_HEADER = "User-Agent";
   private static final ExecutorService executor = Executors.newFixedThreadPool(10);
+  private static String directory;
 
   public static void main(String[] args) {
     try (ServerSocket serverSocket = new ServerSocket(4221)) {
-      String directory = args[1];
+      if (args.length > 1) {
+        directory = args[1];
+      }
 
       serverSocket.setReuseAddress(true);
       while (true) {
         Socket clientSocket = serverSocket.accept();
         executor.submit(() -> {
           try {
-            handleRequest(clientSocket, directory);
+            handleRequest(clientSocket);
           } catch (IOException e) {
             throw new RuntimeException(e);
           }
@@ -38,11 +41,11 @@ public class Main {
     }
   }
 
-  private static void handleRequest(Socket clientSocket, String directory) throws IOException {
+  private static void handleRequest(Socket clientSocket) throws IOException {
     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
     HttpRequest httpRequest = parseRequest(in);
     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-    createResponse(out, httpRequest, directory);
+    createResponse(out, httpRequest);
   }
 
   private static HttpRequest parseRequest(BufferedReader in) throws IOException {
@@ -77,23 +80,31 @@ public class Main {
     return headers;
   }
 
-  private static void createResponse(PrintWriter out, HttpRequest httpRequest, String directory)
+  private static void createResponse(PrintWriter out, HttpRequest httpRequest)
       throws IOException {
     if ("/".equals(httpRequest.uri)) {
       out.println("HTTP/1.1 200 OK\r\n\r\n");
     } else if (httpRequest.uri.startsWith("/echo/")) {
       String pathVariable = httpRequest.uri.replace("/echo/", "");
       out.println(
-          "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + pathVariable.length() + "\r\n\r\n" + pathVariable);
+          "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + pathVariable.length()
+              + "\r\n\r\n" + pathVariable);
     } else if (httpRequest.uri.startsWith("/user-agent")) {
       var headerValue = httpRequest.httpHeaders.get(USER_AGENT_HEADER);
       out.println(
-          "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + headerValue.length() + "\r\n\r\n" + headerValue);
+          "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + headerValue.length()
+              + "\r\n\r\n" + headerValue);
     } else if (httpRequest.uri.startsWith("/files/")) {
-      String pathVariable = httpRequest.uri.replace("/files/", "");
-      List<String> strings = Files.readAllLines(Path.of(directory, pathVariable));
-      String content = strings.get(0);
-      out.println("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + content.length() + "\r\n\r\n" + content);
+      String fileName = httpRequest.uri.replace("/files/", "");
+      File file = new File(directory, fileName);
+      if (file.exists()) {
+        byte[] fileContent = Files.readAllBytes(file.toPath());
+        var content = new String(fileContent);
+        out.println("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: "
+            + content.length() + "\r\n\r\n" + content);
+      } else {
+        out.println("HTTP/1.1 404 Not Found\r\n\r\n");
+      }
     } else {
       out.println("HTTP/1.1 404 Not Found\r\n\r\n");
     }
