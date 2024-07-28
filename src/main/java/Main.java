3ba5772e59036059129e16a655ea73cb73d2402c
danielpopusoi <daniel.popusoi96@gmail.com>
2024-07-28 21:40:33 +0300

    Read header

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,13 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
+
+  private static final String USER_AGENT_HEADER = "User-Agent";
 
   public static void main(String[] args) {
     Socket clientSocket;
@@ -17,26 +21,20 @@ public class Main {
       clientSocket = serverSocket.accept(); // Wait for connection from the client.
 
       BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      List<String> requestData = readRequest(in);
-      String path = requestData.get(0);
-      String[] uriInfo = path.split(" ");
-      HttpRequest httpRequest = new HttpRequest(uriInfo[0], uriInfo[1]);
+      HttpRequest httpRequest = parseRequest(in);
       PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-      writeResponseByPath(out, httpRequest);
+      createResponse(out, httpRequest);
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
   }
 
-  private static void writeResponseByPath(PrintWriter out, HttpRequest httpRequest) {
-    if ("/".equals(httpRequest.uri)) {
-      out.println("HTTP/1.1 200 OK\r\n\r\n");
-    } else if (httpRequest.uri.startsWith("/echo/")) {
-      String pathVariable = httpRequest.uri.replace("/echo/", "");
-      out.println("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + pathVariable.length() + "\r\n\r\n" + pathVariable);
-    } else {
-      out.println("HTTP/1.1 404 Not Found\r\n\r\n");
-    }
+  private static HttpRequest parseRequest(BufferedReader in) throws IOException {
+    List<String> requestData = readRequest(in);
+    String path = requestData.get(0);
+    String[] uriInfo = path.split(" ");
+    Map<String, String> headers = findKnownHeaders(requestData);
+    return new HttpRequest(uriInfo[0], uriInfo[1], headers);
   }
 
   private static List<String> readRequest(BufferedReader in) throws IOException {
@@ -52,14 +50,41 @@ public class Main {
     return requestData;
   }
 
+  private static Map<String, String> findKnownHeaders(List<String> requestData) {
+    Map<String, String> headers = new HashMap<>();
+    for (var data : requestData) {
+      if (data.startsWith(USER_AGENT_HEADER)) {
+        var headerValue = data.split(": ")[1];
+        headers.put(USER_AGENT_HEADER, headerValue);
+      }
+    }
+    return headers;
+  }
+
+  private static void createResponse(PrintWriter out, HttpRequest httpRequest) {
+    if ("/".equals(httpRequest.uri)) {
+      out.println("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (httpRequest.uri.startsWith("/echo/")) {
+      String pathVariable = httpRequest.uri.replace("/echo/", "");
+      out.println("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + pathVariable.length() + "\r\n\r\n" + pathVariable);
+    } else if (httpRequest.uri.startsWith("/user-agent")) {
+      var headerValue = httpRequest.httpHeaders.get(USER_AGENT_HEADER);
+      out.println("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + headerValue.length() + "\r\n\r\n" + headerValue);
+    } else {
+      out.println("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+  }
+
   private static class HttpRequest {
 
     String httpMethod;
     String uri;
+    Map<String, String> httpHeaders;
 
-    HttpRequest(String httpMethod, String uri) {
+    HttpRequest(String httpMethod, String uri, Map<String, String> httpHeaders) {
       this.httpMethod = httpMethod;
       this.uri = uri;
+      this.httpHeaders = httpHeaders;
     }
 
   }
